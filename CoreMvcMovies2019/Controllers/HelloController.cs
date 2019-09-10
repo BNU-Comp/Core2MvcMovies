32348061933a284dd8f34b77567b688bf7d23fab
DerekPeacock <derek.peacock@yahoo.com>
2019-09-10 19:48:40 +0100

    Added a hello controller

---
+++ CoreMvcMovies2019/Controllers/HelloController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreMvcMovies2019.Controllers
+{
+    public class HelloController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+
+        public string Index()
+        {
+            return "Hello -> This is my default action...";
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public string Welcome(string name, int numTimes = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+            //return "Hello -> This is the Welcome action method...";
+        }
+    }
+}
